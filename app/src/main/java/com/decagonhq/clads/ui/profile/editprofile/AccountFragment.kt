4de231b050b0f0a3aaa0e5a97362b9d52d86dda9
Unoka Ruth <33810711+Ruthy4@users.noreply.github.com>
2021-08-17 21:39:37 +0100

    fix ktlint error

--- app/src/main/java/com/decagonhq/clads/ui/profile/editprofile/AccountFragment.kt
+++ app/src/main/java/com/decagonhq/clads/ui/profile/editprofile/AccountFragment.kt
@@ -123,7 +123,7 @@ class AccountFragment : BaseFragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = AccountFragmentBinding.inflate(inflater, container, false)
         return binding.root
