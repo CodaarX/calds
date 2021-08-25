8787c28f6339721553f20245a11e1d1a6275651f
Unoka Ruth <33810711+Ruthy4@users.noreply.github.com>
2021-08-25 14:00:27 +0100

    created client chat recyclerview and chat fragment

--- app/src/main/java/com/decagonhq/clads/ui/profile/adapter/MessagesFragmentClientsRecyclerAdapter.kt
+++ app/src/main/java/com/decagonhq/clads/ui/profile/adapter/MessagesFragmentClientsRecyclerAdapter.kt
@@ -2,18 +2,32 @@ package com.decagonhq.clads.ui.profile.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.amulyakhare.textdrawable.TextDrawable
+import com.decagonhq.clads.R
 import com.decagonhq.clads.data.domain.MessagesNotificationModel
+import com.decagonhq.clads.databinding.ChatRecyclerviewItemBinding
 import com.decagonhq.clads.databinding.MessagesRecyclerViewItemBinding
+import com.decagonhq.clads.util.ColorSelector
+import java.util.*
+import kotlin.collections.ArrayList
 
-class MessagesFragmentClientsRecyclerAdapter(private var messageNotificationList: ArrayList<MessagesNotificationModel>) : RecyclerView.Adapter<MessagesFragmentClientsRecyclerAdapter.ViewHolder>() {
-    inner class ViewHolder(val binding: MessagesRecyclerViewItemBinding) : RecyclerView.ViewHolder(binding.root) {
-        val clientName = binding.messagesFragmentClientNameTextView
-        val notificationBody = binding.messagesFragmentLoremIpsumTextView
+class MessagesFragmentClientsRecyclerAdapter(
+    private var messageNotificationList: ArrayList<MessagesNotificationModel>
+) :
+    RecyclerView.Adapter<MessagesFragmentClientsRecyclerAdapter.ViewHolder>() {
+
+    inner class ViewHolder(val binding: ChatRecyclerviewItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val clientName = binding.chatRecyclerViewItemClientNameTextView
+        val notificationBody = binding.chatRecyclerViewItemMessageTextView
     }
+
     /*inflate the views*/
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = MessagesRecyclerViewItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            ChatRecyclerviewItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
@@ -24,14 +38,34 @@ class MessagesFragmentClientsRecyclerAdapter(private var messageNotificationList
 
     /*bind the views with their holders*/
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
         holder.itemView.apply {
             with(holder) {
                 with(messageNotificationList[position]) {
                     val clientFullName = "$firstName $lastName"
                     clientName.text = clientFullName
                     notificationBody.text = body
+
+                    val clientInitials = clientFullName.split(" ")[0].substring(0, 1).capitalize(
+                        Locale.ROOT
+                    ) +
+                            clientFullName.split(" ")[1].substring(0, 1).capitalize(Locale.ROOT)
+                    val color =
+                        ColorSelector.selectColorByCharacter((clientFullName.split(" ")[0].first()))
+                    val drawable = TextDrawable.builder().beginConfig()
+                        .width(150)
+                        .height(150)
+                        .fontSize(55)
+                        .endConfig()
+                        .buildRound(clientInitials, color)
+
+                    binding.chatRecyclerViewItemImageView.setImageDrawable(drawable)
+                    binding.chatRecyclerViewItemParentLayout.setOnClickListener {
+                        findNavController().navigate(R.id.clientChatFragment)
+                    }
                 }
             }
         }
     }
 }
+
